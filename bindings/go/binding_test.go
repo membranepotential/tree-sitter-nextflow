4f580a8538825156452f716bb9045d865ed44ec8
Felix Peppert <felix.peppert@pm.me>
2025-06-24 18:53:42 +0200

    Update bindings and Makefile with tree-sitter generate

--- bindings/go/binding_test.go
+++ bindings/go/binding_test.go
@@ -1,15 +1,15 @@
-package tree_sitter_groovy_test
+package tree_sitter_nextflow_test
 
 import (
 	"testing"
 
-	tree_sitter "github.com/smacker/go-tree-sitter"
-	"github.com/tree-sitter/tree-sitter-groovy"
+	tree_sitter "github.com/tree-sitter/go-tree-sitter"
+	tree_sitter_nextflow "github.com/matthuska/tree-sitter-nextflow/bindings/go"
 )
 
 func TestCanLoadGrammar(t *testing.T) {
-	language := tree_sitter.NewLanguage(tree_sitter_groovy.Language())
+	language := tree_sitter.NewLanguage(tree_sitter_nextflow.Language())
 	if language == nil {
-		t.Errorf("Error loading Groovy grammar")
+		t.Errorf("Error loading Nextflow grammar")
 	}
 }
--- bindings/node/binding.cc
+++ bindings/node/binding.cc
@@ -2,19 +2,18 @@
 
 typedef struct TSLanguage TSLanguage;
 
-extern "C" TSLanguage *tree_sitter_groovy();
+extern "C" TSLanguage *tree_sitter_nextflow();
 
 // "tree-sitter", "language" hashed with BLAKE2
 const napi_type_tag LANGUAGE_TYPE_TAG = {
-  0x8AF2E5212AD58ABF, 0xD5006CAD83ABBA16
+    0x8AF2E5212AD58ABF, 0xD5006CAD83ABBA16
 };
 
 Napi::Object Init(Napi::Env env, Napi::Object exports) {
-    exports["name"] = Napi::String::New(env, "groovy");
-    auto language = Napi::External<TSLanguage>::New(env, tree_sitter_groovy());
+    auto language = Napi::External<TSLanguage>::New(env, tree_sitter_nextflow());
     language.TypeTag(&LANGUAGE_TYPE_TAG);
     exports["language"] = language;
     return exports;
 }
 
-NODE_API_MODULE(tree_sitter_groovy_binding, Init)
+NODE_API_MODULE(tree_sitter_nextflow_binding, Init)
